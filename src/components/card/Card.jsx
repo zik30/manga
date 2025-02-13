d7bdfb5938837c7d39cac9a179bf4cdf24394809
zik30 <ainazik.ishenbek@gmail.com>
2025-02-14 01:30:47 +0600

    css commit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,15 @@
 import styles from './card.module.css';
 
 const Card = ({img, title, chapter})=>{
-    console.log(img);
+    console.log(chapter);
     
 
 
     return(
         <div className={styles.card}>
-            <img src={img} alt={title} />
+            <img src={img} alt={title} />            
             <h3 className={styles.title}>{title}</h3>
-            <p className={styles.chapter}>{chapter}</p>
+            <p className={styles.chapter}>{chapter ? chapter : "N / A"}</p>
         </div>
     )
 }
